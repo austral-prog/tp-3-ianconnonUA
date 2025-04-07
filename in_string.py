d885d937a034058df415200f535edfa19a4db53f
ianconnon <ianvconnong@gmail.com>
2025-04-07 11:08:54 -0300

    Entrega tp 3

--- in_string.py
+++ in_string.py
@@ -1,6 +1,22 @@
 def check_vowels():
-    # Código a implementar utilizando input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    name = input(" ").lower()
+    if 'a' in name:
+        print("Contiene a: True")
+    elif 'a' not in name:
+        print("Contiene a: False")
+    if 'e' in name:
+        print("Contiene e: True")
+    elif 'e' not in name:
+        print("Contiene e: False")
+    if 'i' in name:
+        print("Contiene i: True")
+    elif 'i' not in name:
+        print("Contiene i: False")
+    if 'o' in name:
+        print("Contiene o: True")
+    elif 'o' not in name:
+        print("Contiene o: False")
+    if 'u' in name: 
+        print("Contiene u: True")
+    elif 'u' not in name:
+        print("Contiene u: False")
